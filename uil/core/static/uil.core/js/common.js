71f9393e8db1c8172b274c1e816e89d727d9b378
T.D. Mees <t.d.mees@uu.nl>
2018-11-29 11:00:38 +0100

    Refactored common.js and added help_text to qTip code

--- uil/core/static/uil.core/js/common.js
+++ uil/core/static/uil.core/js/common.js
@@ -1,4 +1,5 @@
 $(function() {
+    // Turns al tables with the dt class into datatable tables
     $('table.dt').DataTable( {
         dom: 'Bfrtip',
         buttons : [
@@ -15,8 +16,29 @@ $(function() {
         responsive: true,
         paginationType: "full_numbers",
     } );
-});
 
-$(document).ready(function() {
+    // Enables select2 for all select boxes in forms
     $('form table select').select2();
+
+    // Turns all help text into nice (i) icons with qTip hover-text
+    $("[id^=id]").each(function() {
+        let help = $(this).nextAll('.helptext');
+        if (help.html())
+        {
+            let label = $("th label[for^='" + $(this).attr('id') + "']").first();
+            let icon = $('<span class="icon-info"></span');
+            icon.html('&nbsp;');
+            icon.appendTo(label);
+            icon.qtip({
+                content: {
+                    text: help.html(),
+                },
+                hide: {
+                    fixed: true,
+                    delay: 500,
+                },
+            });
+            help.remove();
+        }
+    });
 });
