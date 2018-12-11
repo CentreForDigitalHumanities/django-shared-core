352341d57f2a3bb3050c68aa7930687639cb54e0
T.D. Mees <t.d.mees@uu.nl>
2018-12-11 11:12:32 +0100

    Fixed the RedirectActionView not properly returning a response

--- uil/core/views/base.py
+++ uil/core/views/base.py
@@ -2,6 +2,12 @@ from django.views.generic import RedirectView
 
 
 class RedirectActionView(RedirectView):
+    """This base view extends generic.RedirectView by adding a new method
+    that is called before getting the redirect url.
+
+    To be used for redirectViews that handle an action during it's request,
+    as an alternative to doing that stuff in get_redirect_url. (It's cleaner)
+    """
 
     def action(self, request):
         """
@@ -11,4 +17,4 @@ class RedirectActionView(RedirectView):
 
     def get(self, request, *args, **kwargs):
         self.action(request)
-        super(RedirectActionView, self).get(request, *args, **kwargs)
+        return super(RedirectActionView, self).get(request, *args, **kwargs)
