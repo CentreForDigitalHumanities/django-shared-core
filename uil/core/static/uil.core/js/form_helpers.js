13cd48a236352cb1b048725e4dcf7b381f4e9f5a
T.D. Mees <t.d.mees@uu.nl>
2019-01-31 14:18:03 +0100

    Added add_title js function + required styles

--- uil/core/static/uil.core/js/form_helpers.js
+++ uil/core/static/uil.core/js/form_helpers.js
@@ -1,9 +1,16 @@
 function depends_on_value(a, a_value, b)
 {
     $('input[name=' + a + ']').change(function() {
-        var check = $('input[name=' + a + ']:checked').val() === a_value;
+        let check = $('input[name=' + a + ']:checked').val() === a_value;
 
         $('#id_' + b).parents('tr').toggle(check);
     });
     $('input[name=' + a + ']').change();
+}
+
+function add_title(field, title)
+{
+    let insert = $('<tr class="no-background">').append($('<th>').append($('<h3>').text(title))).append($('<td>'));
+    $('#id_' + field).parents('tr').before(insert);
+    console.log("#id_"+field)
 }
