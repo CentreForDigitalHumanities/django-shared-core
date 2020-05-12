8acd6968014b3a501542e786e68a5e8820ad5e09
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2020-05-12 12:09:30 +0200

    Moved the encrypted fields to UiL Core

---
+++ uil/core/fields/mixin.py
@@ -0,0 +1,49 @@
+"""
+Adapted from django-encrypted-model-fields by
+"""
+from __future__ import unicode_literals
+
+import cryptography.fernet
+from django.utils.six import string_types
+
+from .crypter import decrypt_str, encrypt_str
+
+
+class EncryptedMixin(object):
+    def to_python(self, value):
+        if value is None:
+            return value
+
+        if isinstance(value, (bytes, string_types[0])):
+            if isinstance(value, bytes):
+                value = value.decode('utf-8')
+            try:
+                value = decrypt_str(value)
+            except cryptography.fernet.InvalidToken:
+                pass
+
+        return super(EncryptedMixin, self).to_python(value)
+
+    def from_db_value(self, value, expression, connection):
+        return self.to_python(value)
+
+    def get_db_prep_save(self, value, connection):
+        value = super(EncryptedMixin, self).get_db_prep_save(value, connection)
+
+        if value is None:
+            return value
+
+        # decode the encrypted value to a unicode string, else this breaks in pgsql
+        return (encrypt_str(str(value))).decode('utf-8')
+
+    def get_internal_type(self):
+        return "TextField"
+
+    def deconstruct(self):
+        name, path, args, kwargs = super(EncryptedMixin, self).deconstruct()
+
+        if 'max_length' in kwargs:
+            del kwargs['max_length']
+
+        return name, path, args, kwargs
+
