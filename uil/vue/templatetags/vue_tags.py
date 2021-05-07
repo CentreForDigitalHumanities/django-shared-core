bed2dc368e6173bc9ef02032b9b436e8dd7a8aff
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2021-05-07 11:22:49 +0200

    strict CSP compatibility

--- uil/vue/templatetags/vue_tags.py
+++ uil/vue/templatetags/vue_tags.py
@@ -9,20 +9,25 @@ register = template.Library()
 
 
 @register.simple_tag
-def load_vue_component(root_component):
+def load_vue_component(root_component, nonce=''):
     comp = Vue.get_component(root_component)
 
+    if nonce:
+        nonce = f"nonce='{nonce}'"
+
     output = ""
-    output += '<script src="{}"></script>\n'.format(
-        reverse('uil.vue:vue-js', args=[root_component, ])
+    output += '<script src="{}" {}></script>\n'.format(
+        reverse('uil.vue:vue-js', args=[root_component, ]),
+        nonce
     )
     output += '<link href="{}" rel="stylesheet" />\n'.format(
         reverse('uil.vue:vue-css', args=[root_component, ])
     )
 
     for template_id, template in get_vue_templates(comp):
-        output += '<script type="text/x-template" id="{}">{}</script>\n'.format(
+        output += '<script type="text/x-template" id="{}" {}>{}</script>\n'.format(
             template_id,
+            nonce,
             template,
         )
 
