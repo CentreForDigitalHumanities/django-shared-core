ff1a4bdd7b1353e35655ce00de770f882d5f2d0b
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2021-08-10 11:47:36 +0200

    Made making migrations possible without a Django project

---
+++ helpers/makemigrations.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+"""As this project doesn't contain a django project, we can't make migrations
+the normal way. This script solves this problem by executing the
+makemigrations command in a minimal django environment"""
+from django.core.management import call_command
+
+import _setup_env
+
+_setup_env.setup_environment()
+call_command('makemigrations')
