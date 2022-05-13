cc178c418a1607b441b610df4ba77118bc34499d
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2022-05-13 11:17:36 +0200

    Updated setup.py - Reflected name change - Updated supported django versions - Added DH-IT Portal development as author

--- setup.py
+++ setup.py
@@ -4,11 +4,11 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="UiL Django Core",
-    version="2.1.0",
-    author="UiL OTS Labs",
-    author_email="labbeheer.gw@uu.nl",
-    description="A shared code library for UiL OTS Django projects",
+    name="CDH Django Core",
+    version="3.0.0",
+    author="DH-IT Portal development, UiL OTS Labs",
+    author_email="portaldev.gw@uu.nl, labbeheer.gw@uu.nl",
+    description="A shared code library for DH-IT & UiL OTS Django projects",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/UiL-OTS-labs/django-shared-core",
@@ -28,7 +28,8 @@ setuptools.setup(
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "Framework :: Django :: 2.2",
+        "Framework :: Django :: 3.2",
+        "Framework :: Django :: 4.0",
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
     ],
