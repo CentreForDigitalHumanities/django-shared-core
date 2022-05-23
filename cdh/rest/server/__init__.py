064e4cb98e2d794623830cf68854baf612bd2765
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2022-05-23 16:49:50 +0200

    Move ModelDisplaySerializer to cdh.rest app

--- cdh/rest/server/__init__.py
+++ cdh/rest/server/__init__.py
@@ -1,4 +1,5 @@
 from .authenticators import JwtAuthentication
 from .permissions import IsPermittedClient
-import cdh.rest.server.settings as server_settings
+import cdh.rest.server.serializers as serializers
+import cdh.rest.server.settings as settings
 from .token import JwtToken, Algorithms
