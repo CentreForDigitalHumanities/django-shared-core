204d79bd50a984a84ecc9113120f73a38534a881
Mees, T.D. (Ty) <t.d.mees@uu.nl>
2022-06-02 17:52:45 +0200

    Fixed missing middleware

--- dev/dev_project/settings.py
+++ dev/dev_project/settings.py
@@ -77,6 +77,7 @@ MIDDLEWARE = [
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'impersonate.middleware.ImpersonateMiddleware',
     'cdh.core.middleware.ThreadLocalUserMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
